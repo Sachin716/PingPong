b887fdd4f19f7e291e6c1f5dffaef564d212ca25
sachin716 <indegamer1@gmail.com>
2024-11-30 19:14:19 +0530

    Ping Pong Game

---
+++ app/Game/Ai/page.jsx
@@ -0,0 +1,277 @@
+'use client';
+
+
+import React, { useState, useEffect, useRef } from 'react';
+import useSound from 'use-sound';
+
+const localgame = () => {
+  const isHit = useRef(false)
+  const isGame = useRef(false)
+  const isGround = useRef(false)
+  const isWin = useRef(false)
+  const [lPaddleHeight, setLPaddleHeight] = useState(125);
+  const lPaddleRef = useRef(125)
+  const [rPaddleHeight, setRPaddleHeight] = useState(125);
+  const rPaddleRef = useRef(125)
+  const isPlaying  = useRef(false)
+  const [ballPosition , setBallPosition] = useState({
+    v : window.innerHeight/2,
+    h : window.innerWidth /2
+  })
+
+  const ballPositionref = useRef({
+    v : window.innerHeight/2,
+    h : window.innerWidth /2
+  })
+  const ballVelocity = useRef({
+    v : 0,
+    h : 0
+  })
+  const [lScore, setLScore] = useState(0);
+  const lRef = useRef(0)
+  const [rScore, setRScore] = useState(0);
+  const rRef = useRef(0)
+
+
+  useEffect(()=>{
+    let velh = Math.round(Math.max((Math.random()- 0.5) * 600) , 300 )
+    let velv = Math.round(Math.max((Math.random()- 0.5) * 600) , 400 )
+    ballVelocity.current.v = velv
+    ballVelocity.current.h =velh
+
+  } , [])
+  
+
+  const paddleMoveVelocity = 2400;
+  const keystats = useRef({
+    w: false,
+    s: false,
+    up: false,
+    down: false,
+  });
+
+  let lasttime = new Date().getTime();
+
+  const updateKeyStats = (e, value) => {
+    if (e.keyCode === 87) keystats.current.w = value; // W key
+    if (e.keyCode === 83) keystats.current.s = value; // S key
+    if (e.keyCode === 32) isPlaying.current = true
+    if (e.keyCode === 27) isPlaying.current = false
+  };
+
+
+
+  const aiplay = () => {
+    const ball = ballPositionref.current
+    const rpaddle = rPaddleRef.current
+
+    if(ball.v <= rpaddle  - 25 && ball.h >= window.innerWidth/2){
+      keystats.current.up = true
+    }
+    else if(ball.v >= rpaddle+125 && ball.h >= window.innerWidth/2){
+      keystats.current.down= true 
+    }
+    else{
+      keystats.current.up = false
+      keystats.current.down = false
+    }
+
+
+    
+  }
+
+  
+  const playloop = () => {
+    const curr = new Date().getTime();
+    const dt = curr - lasttime;
+    lasttime = curr;
+
+    
+
+
+
+    
+    
+
+    if (keystats.current.w) {
+      const newHeight = Math.max(0, lPaddleRef.current - (paddleMoveVelocity / (1000 / dt)));
+      lPaddleRef.current = newHeight;
+      setLPaddleHeight(newHeight);
+    }
+
+    if (keystats.current.s) {
+      const newHeight = Math.min(window.innerHeight-125, lPaddleRef.current + (paddleMoveVelocity / (1000 / dt)));
+      lPaddleRef.current = newHeight;
+      setLPaddleHeight(newHeight);
+    }
+    
+    if (keystats.current.up) {
+      const newHeight = Math.max(0, rPaddleRef.current - (paddleMoveVelocity / (1000 / dt)));
+      rPaddleRef.current = newHeight;
+      setRPaddleHeight(newHeight);
+    }
+    
+    if (keystats.current.down) {
+      const newHeight = Math.min(window.innerHeight-125, rPaddleRef.current + (paddleMoveVelocity / (1000 / dt)));
+      rPaddleRef.current = newHeight;
+      setRPaddleHeight(newHeight);
+    }
+
+    const ball = ballPositionref.current
+
+    if(ball.h > window.innerWidth - 25){
+      const calcLsc = lRef.current + 1
+      lRef.current = calcLsc
+      setLScore(lRef.current) 
+      let velh = Math.round(Math.max((Math.random()- 0.5) * 600) , 300 )
+      let velv = Math.round(Math.max((Math.random()- 0.5) * 600) , 400 )
+      ballVelocity.current.v = velv
+      ballVelocity.current.h = velh
+      ball.h = window.innerWidth/2
+      ball.v = window.innerHeight/2
+      isPlaying.current = false
+      isGame.current.play()
+    }
+    if(ball.h <=  1){
+      const calcRsc = rRef.current + 1
+      rRef.current = calcRsc
+      setRScore(rRef.current) 
+      let velh = Math.round(Math.max((Math.random()- 0.5) * 600) , 300 )
+      let velv = Math.round(Math.max((Math.random()- 0.5) * 600) , 400 )
+      ballVelocity.current.v = velv
+      ballVelocity.current.h = velh
+      ball.h = window.innerWidth/2
+      ball.v = window.innerHeight/2
+      isPlaying.current = false
+      isGame.current.play()
+    }
+
+    const vel = ballVelocity.current
+
+    if(ball.v > window.innerHeight -25 && vel.v > 0){
+      vel.v = -1*vel.v
+      isGround.current.play() 
+    }
+    if(ball.v <= 0 && vel.v < 0){
+      vel.v = -1*vel.v
+      isGround.current.play()
+      
+    }
+
+    const lpaddle = lPaddleRef.current
+    if(ball.h >= 15 && ball.h <= 30 && ball.v >= lpaddle -25 && ball.v <= lpaddle + 125  && vel.h < 0){
+      vel.h = vel.h*-1.1
+      vel.v = vel.v*1.1
+      isHit.current.play()
+    }
+    const rpaddle = rPaddleRef.current
+
+    
+
+    if(ball.h <= window.innerWidth - 40 && ball.h >= window.innerWidth - 55 && ball.v >= rpaddle -25 && ball.v <= rpaddle + 125  && vel.h > 0){
+      vel.h = vel.h*-1.1
+      vel.v = vel.v*1.1
+      isHit.current.play()
+    }
+
+    aiplay()
+ 
+    if(lRef >= 10){
+      isPlaying.current = false 
+      alert("Player 2 Wins")
+      isWin.current.play()
+    }
+
+    
+    if(rRef >= 10){
+      isPlaying.current = false 
+      alert("Player 1 Wins")
+      isWin.current.play()
+    }
+
+    if(isPlaying.current){    
+    const newHPos = ball.h + vel.h/(1000/dt)
+    const newVPos = ball.v + vel.v/(1000/dt)
+    ballPositionref.current.h = newHPos
+    ballPositionref.current.v = newVPos
+
+    setBallPosition({
+      ...ballPositionref.current
+    })
+  }
+
+    requestAnimationFrame(playloop);
+  };
+
+  useEffect(() => {
+    playloop();
+  }, []);
+ 
+
+  useEffect(() => {
+    const handleKeyDown = (e) => updateKeyStats(e, true);
+    const handleKeyUp = (e) => updateKeyStats(e, false);
+
+    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('keyup', handleKeyUp);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('keyup', handleKeyUp);
+    };
+  }, []);
+
+  return (
+    <div className='w-screen h-screen flex flex-col items-center overflow-hidden'>
+
+      <div className='  flex w-[50%] h-[50%]'>
+        <div className='flex-1 flex items-end text-[150px]  text-zinc-800'>
+          {lScore}
+        </div>
+        <div className=' flex-1 flex items-end justify-end text-[150px] text-zinc-800'>
+          {rScore}
+        </div>
+      </div>
+
+      <div className={`left-paddle absolute w-[20px] h-[125px] bg-white left-[10px]`}  style={{
+        top:`${lPaddleHeight}px`
+      }}  >
+      </div>
+      <div className='ball absolute h-[25px] w-[25px] rounded-full bg-white' style={{ 
+        top:`${ballPosition.v}px`,
+        left:`${ballPosition.h}px`
+       }} >
+      </div>
+      <div className='right-paddle absolute w-[20px] h-[125px] bg-white right-[10px]' style={{top:`${rPaddleHeight}px`}}>
+      </div>
+      <audio ref={isHit}  src={'/groundHit.mp3'} />
+      <audio ref={isWin}  src={'/Gameover.mp3'} />
+      <audio ref={isGround}  src={'/groundHit.mp3'} />
+      <audio ref={isGame}  src={'/Score.mp3'} />
+
+
+      {isPlaying.current ? "" :  <div className='w-screen fixed h-screen flex items-center align-center text-center justify-center  px-auto py-auto bg-[#00000011] m-0'>
+          controls <br></br><br></br>
+          Left Paddle   <br></br>
+          W : up        <br></br>
+          s : down      <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          Right Paddle <br></br>
+          up arrow : up<br></br>
+          down arrow : down<br></br>
+          <br></br>
+          <br></br>
+          <br></br> 
+          esc to pause <br></br>
+          <br></br>
+          Space to continue Game
+
+        </div>}
+      
+    </div>
+  )
+}
+
+export default localgame
---
+++ app/page.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import React from 'react'
+import { useRouter } from 'next/navigation'
+
+const mainmenu = () => {
+  const router = useRouter()
+  
+  return (
+    <div className=' w-sceen h-screen py-auto flex items-center bg-zinc-950 '>
+      <div className='m-auto flex flex-col bg-zinc-900 w-[50%] h-[50%] items-center rounded-xl '>
+        <div className='w-full m-auto flex flex-col items-center'>
+        <button className='w-[90%] h-12 mt-4 p-auto border-zinc-600 border-solid border-2 rounded-lg hover:bg-slate-950 ' onClick={() => {router.push('/Game/Ai')}}>
+          Against Ai
+        </button>
+        <button className='w-[90%] h-12 mt-4 p-auto border-zinc-600 border-solid border-2 rounded-lg hover:bg-slate-950' onClick={() => {router.push('/Game/local')}}>
+          Online Multiplayer
+        </button>
+        <button className='w-[90%] h-12 mt-4 p-auto border-zinc-600 border-solid border-2 rounded-lg hover:bg-slate-950'>
+          Online Multiplayer
+        </button>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default mainmenu
